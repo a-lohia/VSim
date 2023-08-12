e670717d206542fd4e014d08df3db0ef35cb1509
aryalohia <aryalohia00@gmail.com>
2023-08-12 14:18:43 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+# This file is used to simulate differential motion. The user inputs a pathway using mouse input
+# and the program will generate a video of the pathway with a moving square that follows the path
+# and a constant moving background. The program will also generate a reference video of the same
+# path with just the outline of the moving square for comparison later.
+
+# go to data_producer folder to adjust the parameters of the produced video (size, length, etc...)
+
+from data_producer.oms_data_producer import *
+from data_producer.path_producer import *
+import numpy as np
+import yaml
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from pathlib import Path
+import skvideo.io
+
+
+# -----------------------
+# --- Params / Setup ----
+# -----------------------
+
+# tk.Tk().withdraw()  # part of the import if you are not using other tkinter functions
+# Ask for file (GUI)
+# fn = Path(askopenfilename())
+fn = Path(r"data_producer/data_producer_params.yaml")
+
+PARAMS_DIR = fn
+
+with open(PARAMS_DIR) as f:
+    params = yaml.safe_load(f)
+
+    save_dir = params["save_dir"]
+    ref_save_dir = params["ref_save_dir"]
+    fps = params["fps"]
+    height = params["height"]
+    width = params["width"]
+    frames = params["frames"]
+    col_width = params["col_width"]
+    object_radius = params["object_radius"]
+
+seconds = np.ceil(frames / fps).astype(int)
+
+canvas = np.zeros(shape=(frames, height, width), dtype=np.int32)
+ref_canvas = np.zeros(shape=(frames, height, width), dtype=np.int32)
+
+# -----------------------
+# -- Paths / Creation ---
+# -----------------------
+
+# change to a path creation function of your choice if preferred
+x, y = manually_create_path(width, height, frames)
+
+canvas = create_moving_background(canvas, fps, seconds)
+
+canvas = apply_object_to_path(canvas, (x, y), object_radius)
+ref_canvas = apply_object_to_path(ref_canvas, (x, y), object_radius, fill=False)
+
+
+# RENDER
+
+result = encode_to_video(canvas, fps, save_dir)
+ref_result = encode_to_video(ref_canvas, fps, ref_save_dir)
+
+if result:
+    print("Video saved successfully!")
+else:
+    print("Error saving video.")
